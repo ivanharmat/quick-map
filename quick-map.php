93f7af2038555e7578110d4071237f7ed495edf7
Ivan Harmat <ivanharmat@yahoo.com>
2019-11-16 19:18:16 -0500

    Final changes before wordpress upload

--- quick-map.php
+++ quick-map.php
@@ -3,7 +3,7 @@
  * Plugin Name: Quick Map
  * Author: Ivan Harmat / Renzo Johnson
  * Plugin URI: https://github.com/ivanharmat/quick-map
- * Description: The easiest google map integration to your website.
+ * Description: The easiest google map integration for your website.
  * Version: 1.0.0
  * Author URI: https://renzojohnson.com/
  * Text Domain: quick-map
@@ -18,50 +18,46 @@ $quickmap = new Quick_Map();
  */
 class Quick_Map {
 
-	/**
-	 * Shortcode tag name.
-	 * @var string
-	 */
-	private $shortcode_tag  = 'quick-map';
+	private $shortcode_tag   = 'quick-map';
 
-	/**
-	 * Default class name.
-	 * @var string
-	 */
-	private $class_name     = 'quickmap';
+	private $default_width   = '100%';
 
-	/**
-	 * Quick_Map constructor.
-	 */
+	private $default_height  = '300px';
+
+	private $default_content = 'Orlando, FL';
+
 	function __construct() {
 		add_action( 'init', array( $this, 'init' ) );
 	}
 
-	/**
-	 * Init function.
-	 */
 	public function init() {
-		// add_action( 'wp_head', array( $this, 'wp_head' ) );
-		add_shortcode( $this->get_shortcode_tag(), array( $this, 'quick_shortcode' ) );
+		add_shortcode( $this->get_quick_shortcode_tag(), array( $this, 'quick_shortcode' ) );
 	}
 
-	/**
-	 * Get shortcode tag.
-	 *
-	 * @return mixed
-	 */
-	private function get_shortcode_tag() {
-
+	private function get_quick_shortcode_tag() {
 		return apply_filters( 'quickmap_shortcode_tag', $this->shortcode_tag );
 	}
 
 	/**
-	 * Output tags at footer.
-	 * @param array $p Width,height,zoom.
-	 * @param null  $content Content.
+	 * Generate iframe code.
+	 * @param array $args.
+	 * @param null  $content Content inside the tags.
 	 * @return string
 	 */
-	public function quick_shortcode($p, $content = null ) {
-		return sprintf('<iframe src="https://www.google.com/maps?q=%1$s&amp;output=embed&amp;title=" ivan""="" height="200" frameborder="0" style="border:0"></iframe>', esc_html( trim( $content)));
+	public function quick_shortcode($args, $content = null ) {
+		if ( isset( $args['width'] ) && preg_match( '/^[0-9]+(%|px)$/', $args['width'] ) ) {
+			$w = $args['width'];
+		} else {
+			$w = apply_filters( 'quickmap_default_width', $this->default_width );
+		}
+		if ( isset( $args['height'] ) && preg_match( '/^[0-9]+(%|px)$/', $args['height'] ) ) {
+			$h = $args['height'];
+		} else {
+			$h = apply_filters( 'quickmap_default_height', $this->default_height );
+		}
+		if(is_null($content) || empty($content) || !$content) {
+			$content = apply_filters( 'quickmap_default_content', $this->default_content);
+		}
+		return sprintf('<iframe src="//www.google.com/maps?q=%1$s&amp;output=embed" frameborder="0" width="%2$s" height="%3$s"></iframe>', esc_html( trim( $content)), esc_attr( $w ), esc_attr( $h ));
 	}
 }
